ecdadf3df6a1c38700b1540b5b43d22105249be0
wjochim <wjochim@uni-bremen.de>
2019-03-09 20:49:18 +0100

    v0.1

---
+++ precache-manifest.25a9673d8cdafd403f51357d465a0314.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a049b25d9181e6c909d",
+    "url": "./static/js/main.c5392d53.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "f8433f70025d9ae553ef",
+    "url": "./static/js/2.c23ce629.chunk.js"
+  },
+  {
+    "revision": "25d7948c00005733c2eaa1958249a0d4",
+    "url": "./index.html"
+  }
+];
